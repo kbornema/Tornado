5c9ee0d16ffb1f882280cc02f650ff660657faf2
Gerd Schmidt <gerd-has@awesome-co.de>
2017-07-02 14:23:53 +0200

    Water_kill plane

---
+++ Assets/Scripts/KillPlane.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlane : MonoBehaviour {
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Tornado t = collision.collider.gameObject.GetComponent<Tornado>();
+        if (t == null)
+        {
+            GameObject.Destroy(collision.gameObject);
+        }
+        else
+        {
+        }
+
+    }
+}
